a7d62aa01b54c9c37f319958770447d278b691f6
toms0910 <psp1070@gmail.com>
2023-10-20 20:24:43 +0900

    Update database.go

--- databaseSys/database.go
+++ databaseSys/database.go
@@ -2,17 +2,18 @@ package databaseSys
 
 import (
 	"PasswordProxy/configSys"
+	"errors"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-type DataBaseSys struct {
+type DataBaseStruct struct {
 	db *gorm.DB
 }
 
 // Connect Connection Database Function
-func Connect(config configSys.DatabaseConfig) (DataBaseSys, error) {
+func Connect(config configSys.DatabaseConfig) (DataBaseStruct, error) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		config.Username,
 		config.Password,
@@ -23,28 +24,36 @@ func Connect(config configSys.DatabaseConfig) (DataBaseSys, error) {
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
-		return DataBaseSys{}, err
+		return DataBaseStruct{}, err
 	}
 
-	return DataBaseSys{
+	return DataBaseStruct{
 		db: db,
 	}, err
 }
 
 // Init InitDatabase
-func (d DataBaseSys) Init() error {
+func (d DataBaseStruct) Init() error {
 	return d.db.AutoMigrate(&User{}, &Crypto{})
 }
 
 //===User==
 
 // CreateUser create
-func (d DataBaseSys) CreateUser(user User) error {
-	return d.db.Create(&user).Error
+func (d DataBaseStruct) CreateUser(user User) error {
+	err := d.db.Model(&User{}).Where("name = ?", user.Username).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		err = d.db.Create(&user).Error
+		if err != nil {
+			return err
+		}
+	}
+
+	return gorm.ErrRegistered
 }
 
 // ReadUser read
-func (d DataBaseSys) ReadUser(id int) (User, error) {
+func (d DataBaseStruct) ReadUser(id int) (User, error) {
 	var user User
 	err := d.db.First(&user, id).Error
 
@@ -56,7 +65,7 @@ func (d DataBaseSys) ReadUser(id int) (User, error) {
 }
 
 // ReadUsers reads
-func (d DataBaseSys) ReadUsers(limit int, offset int) ([]User, error) {
+func (d DataBaseStruct) ReadUsers(limit int, offset int) ([]User, error) {
 	var users []User
 	err := d.db.Order("id").Limit(limit).Offset(offset).Find(&users).Error
 
@@ -68,12 +77,12 @@ func (d DataBaseSys) ReadUsers(limit int, offset int) ([]User, error) {
 }
 
 // UpdateUser update
-func (d DataBaseSys) UpdateUser(id int, user User) error {
+func (d DataBaseStruct) UpdateUser(id int, user User) error {
 	return d.db.Model(&User{}).Where("id = ?", id).Updates(user).Error
 }
 
 // DeleteUser delete
-func (d DataBaseSys) DeleteUser(id int) error {
+func (d DataBaseStruct) DeleteUser(id int) error {
 	return d.db.Delete(&User{}, id).Error
 }
 
@@ -82,12 +91,12 @@ func (d DataBaseSys) DeleteUser(id int) error {
 //===Crypto==
 
 // CreateCrypto create
-func (d DataBaseSys) CreateCrypto(crypt Crypto) error {
+func (d DataBaseStruct) CreateCrypto(crypt Crypto) error {
 	return d.db.Create(&crypt).Error
 }
 
 // ReadCrypto read
-func (d DataBaseSys) ReadCrypto() (Crypto, error) {
+func (d DataBaseStruct) ReadCrypto() (Crypto, error) {
 	var crypto Crypto
 	err := d.db.First(&crypto, 1).Error
 
@@ -99,7 +108,7 @@ func (d DataBaseSys) ReadCrypto() (Crypto, error) {
 }
 
 // UpdateCrypto update
-func (d DataBaseSys) UpdateCrypto(crypt Crypto) error {
+func (d DataBaseStruct) UpdateCrypto(crypt Crypto) error {
 	return d.db.Model(&Crypto{}).Where("id = ?", 1).Updates(crypt).Error
 }
 
