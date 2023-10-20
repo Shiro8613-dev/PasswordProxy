3c67155240fd0f590f622750f8c2bf869438c098
toms0910 <psp1070@gmail.com>
2023-10-21 01:57:51 +0900

    リバースプロキシミドルウェア実装

---
+++ webSys/middleware/proxy.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"PasswordProxy/webSys/settings"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+)
+
+func Proxy(pass string, rewrite bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		remote, err := url.Parse(pass)
+		if err != nil {
+			c.AbortWithStatus(500)
+		}
+
+		proxy := httputil.NewSingleHostReverseProxy(remote)
+		proxy.Director = func(req *http.Request) {
+			req.Header = c.Request.Header
+			req.Host = remote.Host
+			req.URL.Scheme = remote.Scheme
+			req.URL.Host = remote.Host
+			if rewrite {
+				req.URL.Path = strings.Replace(c.Request.URL.Path, settings.ProxiedPath, "", 1)
+			} else {
+				req.URL.Path = c.Request.URL.Path
+			}
+		}
+
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}
+}
