c793d631bc20561753d151c8e24ecd16710464b9
toms0910 <psp1070@gmail.com>
2023-10-21 17:59:32 +0900

    fix

--- webSys/middleware/jwtsys.go
+++ webSys/middleware/jwtsys.go
@@ -1,26 +1,21 @@
 package middleware
 
 import (
-	"PasswordProxy/databaseSys"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"time"
 )
 
 // JwtGenerate generate
-func JwtGenerate(username string, database databaseSys.DataBaseStruct) (string, error) {
+func JwtGenerate(username string, salt string) (string, error) {
 	claims := jwt.MapClaims{
 		"username": username,
 		"exp":      time.Now().Add(time.Hour * 24).Unix(),
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	salt, err := database.ReadCrypto()
-	if err != nil {
-		return "", err
-	}
 
-	accessToken, err := token.SignedString([]byte(salt.Salt))
+	accessToken, err := token.SignedString([]byte(salt))
 	if err != nil {
 		return "", err
 	}
@@ -29,18 +24,14 @@ func JwtGenerate(username string, database databaseSys.DataBaseStruct) (string, 
 }
 
 // JwtVerify verify
-func JwtVerify(accessToken string, database databaseSys.DataBaseStruct) (string, error) {
-	salt, err := database.ReadCrypto()
-	if err != nil {
-		return "", err
-	}
+func JwtVerify(accessToken string, salt string) (string, error) {
 
 	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
-		return []byte(salt.Salt), nil
+		return []byte(salt), nil
 	})
 
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
