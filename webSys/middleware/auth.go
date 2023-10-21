eef35119029721b11b6e8e7752710a53ef788ceb
toms0910 <psp1070@gmail.com>
2023-10-21 15:28:52 +0900

    認証機能の修正

--- webSys/middleware/auth.go
+++ webSys/middleware/auth.go
@@ -13,33 +13,35 @@ import (
 func Auth(admin bool, database databaseSys.DataBaseStruct) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		session := sessions.Default(c)
-		token := session.Get("jwt").(string)
-		if token == "" {
+		token := session.Get("jwt")
+		if token == nil {
 			c.Redirect(http.StatusFound, settings.AuthPath+settings.LoginPagePath)
-		}
-
-		username, err := JwtVerify(token, database)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "jwt error"})
-		}
-
-		user, err := database.FindUser(username)
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			c.Redirect(http.StatusFound, settings.AuthPath+settings.LoginPagePath)
-		}
-
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "user error"})
-		}
-
-		if admin {
-			if user.Admin {
-				c.Next()
-			} else {
-				c.JSON(http.StatusUnauthorized, gin.H{"error": "permission denied"})
-			}
 		} else {
-			c.Next()
+			token := token.(string)
+
+			username, err := JwtVerify(token, database)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "jwt error"})
+			}
+
+			user, err := database.FindUser(username)
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				c.Redirect(http.StatusFound, settings.AuthPath+settings.LoginPagePath)
+			}
+
+			if err != nil {
+				c.JSON(http.StatusUnauthorized, gin.H{"error": "user error"})
+			}
+
+			if admin {
+				if user.Admin {
+					c.Next()
+				} else {
+					c.JSON(http.StatusUnauthorized, gin.H{"error": "permission denied"})
+				}
+			} else {
+				c.Next()
+			}
 		}
 	}
 }
